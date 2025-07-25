ad0fdf771d004005ba58046398e369ba22f9822c
Eddy Ckedar <eddykedar@gmail.com>
2025-07-25 19:38:49 +0200

    Update Layout.tsx

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,10 +1,7 @@
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-
-interface LayoutProps {
-  children: ReactNode;
-}
+import { LayoutProps } from "@/interface"; // adjust path if no alias
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
